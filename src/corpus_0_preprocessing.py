d5deb9564c0568e758c6c93158ec2129869a63e5
Peter Mühleder <peter.muehleder@uni-leipzig.de>
2018-08-13 17:26:46 +0200

    nlp corpus + phrases model added

---
+++ src/corpus_0_preprocessing.py
@@ -0,0 +1,75 @@
+import json
+import re
+from janome.tokenizer import Tokenizer
+from tqdm import tqdm
+
+
+DATA_FILE = "../data/ck.json"
+
+OUT_FILE = "../corpus/corpus_preproc.txt"
+
+
+NUMBERING_RE = re.compile(r'ー[0-9]+ー')
+NUMBERING2_RE = re.compile(r'第.{1,3}回')
+NUMBERING3_RE = re.compile(r'\([0-9]{1,2}\)')
+NUMBERING4_RE = re.compile(r'ー.{1,3}ー')
+PAGING_RE = re.compile(r'《.*枚》')
+PAGING2_RE = re.compile(r' .{1,3}枚')
+
+
+
+def get_titles(filepath):
+    data = json.load(open(filepath))
+    for issue in data["issues"]:
+        for article in issue["toc"]:
+            yield article["title"], issue["year"]
+
+
+t = Tokenizer()
+def tokenize(s):
+    tokens = [ token.base_form for token in t.tokenize(s) if  "記号" not in token.part_of_speech]
+    return tokens
+
+
+def preprocess():
+
+    titles = []
+
+    for title, year in tqdm(get_titles(DATA_FILE)):
+        #print(title)
+
+        title = title.replace("ーー", " ")
+        title = title.replace("ー上ー"," ")
+        title = title.replace("ー下ー"," ")
+        title = title.replace("ー中ー"," ")
+        title = title.replace(" 上 ", " ")
+        title = title.replace(" 下 ", " ")
+        title = title.replace("(", " ")
+        title = title.replace(")", " ")
+        title = title.replace("｠", " ")
+        title = title.replace("｟", " ")
+        title = title.replace("･", " ")
+        title = title.replace("､", " ")
+        title = title.replace("。", " ")
+        title = title.replace('"', " ")
+        title = title.replace("<", " ")
+        title = title.replace(">", " ")
+        title = title.replace(" : ", " ")
+        title = NUMBERING_RE.sub(" ", title)
+        title = NUMBERING2_RE.sub(" ", title)
+        title = NUMBERING3_RE.sub(" ", title)
+        title = NUMBERING4_RE.sub(" ", title)
+        title = PAGING_RE.sub(" ", title)
+        title = PAGING2_RE.sub(" ", title)
+
+        tokens = tokenize(title)
+        titles.append(" ".join(tokens))
+        #print(" ")
+
+
+    with open(OUT_FILE, "w") as f:
+        f.writelines("\n".join(titles))
+
+
+if __name__ == "__main__":
+    preprocess()
---
+++ src/corpus_1_phrase_model.py
@@ -0,0 +1,60 @@
+from gensim.models.phrases import Phraser, Phrases
+from tqdm import tqdm
+
+CORPUS_FILE = "../corpus/corpus_preproc.txt"
+PHRASES_FILE = "../data/phrases.txt"
+
+MODEL_FILE = "../models/bigram.model"
+OUT_FILE = "../corpus/corpus_phrases.txt"
+
+class Corpus:
+    def __iter__(self):
+        with open(CORPUS_FILE, "r") as f:
+            for line in f:
+                yield line.strip().split()
+
+
+def load_phrases():
+    with open(PHRASES_FILE) as f:
+        return [ x.strip() for x in f.readlines() ]
+
+
+def check_phrase_list(phrase_list, tokens):
+
+    for phrase in phrase_list:
+        for i in range(len(tokens)):
+            if i < len(tokens)-1:
+                if phrase.startswith(tokens[i]) and tokens[i+1] in phrase:
+                    tokens[i:i+2] = [ "".join(tokens[i:i+2]) ] 
+    return tokens
+
+
+def remove_under(tokens):
+    return [ tok.replace("_", "") for tok in tokens]
+
+
+def build_phrase_model():
+
+    phrase_list = load_phrases()
+
+    phrases = Phrases(Corpus())
+    bigrams = Phraser(phrases)
+
+    bigrams.save(MODEL_FILE)
+
+    with open(OUT_FILE, "w") as f:
+        for line in tqdm(bigrams[Corpus()]):
+
+            line = remove_under(line)
+            line = check_phrase_list(phrase_list, line)
+
+            f.write("{}\n".format(" ".join(remove_under(line))))
+
+
+
+
+if __name__ == "__main__":
+    build_phrase_model()
+    #phrase_list = load_phrases()
+    #print(check_phrase_list(phrase_list, ["a", "b", "想ひ", "出", "づる", "c"]))
+    
